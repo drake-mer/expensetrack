9f5865e52136ede2643f2a0aadd46afd215d35ce
David Kremer <david.kremer@sewan.fr>
2017-02-03 19:34:13 +0100

    demo_script.sh

---
+++ demo_script.sh
@@ -0,0 +1,18 @@
+cd ..
+
+# clean up
+rm -rf demo
+# clone the repo
+git clone david-kremer demo
+# move into folder
+
+cd demo
+# move to 8080 port
+sed -i s/"localhost:8000"/"localhost:8080"/g ui-js/js/client.js
+
+cd extrack
+python manage.py migrate
+python manage.py createsuperuser
+python manage.py runserver localhost:8080 &
+
+
