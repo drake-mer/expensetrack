8c85aef43abb21078258d7eb5b526b215407e8ce
David Kremer <david.kremer@sewan.fr>
2017-02-04 13:48:55 +0100

    update demo script

--- demo_script.sh
+++ demo_script.sh
@@ -7,6 +7,12 @@ git clone david-kremer demo
 # move into folder
 
 cd demo
+
+pip install -r requirements.txt --user
+for folder in ${HOME}/.local/lib/python*;do 
+    export PYTHONPATH=${PYTHONPATH}:${folder}/site-packages/
+done
+
 # move to 8080 port
 sed -i s/"localhost:8000"/"localhost:8080"/g ui-js/js/client.js
 
